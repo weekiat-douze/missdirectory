7ef5d2a0bacd0321236b9f41e08d9aa966bdd0f1
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 21:10:22 +0800

    Change MkdirCommandParser to throw ParseException

--- src/main/java/org/missdirectory/parser/MkdirCommandParser.java
+++ src/main/java/org/missdirectory/parser/MkdirCommandParser.java
@@ -1,15 +1,16 @@
 package org.missdirectory.parser;
 
 import org.missdirectory.commands.MkdirCommand;
+import org.missdirectory.exceptions.ParseException;
 
 import java.util.regex.Pattern;
 
 public class MkdirCommandParser {
     private static final String regex = "^[a-zA-Z0-9_-]+$";
-    public static MkdirCommand parse(String args) {
+    public static MkdirCommand parse(String args) throws ParseException{
         boolean validName = Pattern.matches(regex, args);
         if (!validName) {
-
+            throw new ParseException("Invalid Directory Name.");
         }
         return new MkdirCommand(args);
     }
