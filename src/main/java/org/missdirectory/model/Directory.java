2618d4a1df393cd4134240839e3b66a9afc331bc
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 16:45:35 +0800

    Add Model: Directory

---
+++ src/main/java/org/missdirectory/model/Directory.java
@@ -0,0 +1,11 @@
+package org.missdirectory.model;
+
+public class Directory {
+    private String directoryName;
+
+
+    public Directory(String directoryName) {
+        this.directoryName = directoryName;
+    }
+
+}
