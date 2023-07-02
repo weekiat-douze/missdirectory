3596b21118df013ff967eb32aa09c51a09fb61fb
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 20:37:37 +0800

    Add abstract Command class

---
+++ src/main/java/org/missdirectory/commands/Command.java
@@ -0,0 +1,16 @@
+package org.missdirectory.commands;
+
+import org.missdirectory.model.CurrentDirectory;
+
+/**
+ * Encapsulates a command containing all the relevant actions/information.
+ */
+public abstract class Command {
+
+    /**
+     * Executes command and returns latest representation of current directory
+     * @return Current directory
+     */
+    public abstract void execute(CurrentDirectory currentDirectory);
+
+}
