a41551082e179536ea3cb9df0d405e2e81abcaf5
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 20:42:00 +0800

    Add ParseException class

---
+++ src/main/java/org/missdirectory/exceptions/ParseException.java
@@ -0,0 +1,12 @@
+package org.missdirectory.exceptions;
+
+/**
+ * Exception that represents unsuccessful parsing.
+ */
+public class ParseException extends Exception {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+}
