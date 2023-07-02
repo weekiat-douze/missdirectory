c706ca362cb0544d846450aa0150a9ad9cf59e72
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 18:49:48 +0800

    Add Main UI

--- src/main/java/org/missdirectory/Main.java
+++ src/main/java/org/missdirectory/Main.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         Scanner reader = new Scanner(System.in);
 
-        reader.next();
+        System.out.println("Hello! I am MissDirectory!\n");
+        String mdMenu = "\n1. View/Modify Directory Templates" +
+                "\n2. Create New Template" +
+                "\n3. Generate Directory Structure" +
+                "\n4. quit\n";
+        System.out.println("What would you like to do today?" + mdMenu);
 
+        String userInput = reader.next();
+        while(!userInput.equals("4") && !userInput.equals("quit")) {
+            switch (userInput) {
+                case "1":
+                    System.out.println("one");
+                    break;
+                case "2":
+                    System.out.println("two");
+                    break;
+                default:
+                    System.out.println("Please select one of the options." + mdMenu);
+            }
+            userInput = reader.next();
+        }
 
+        System.out.println("Good bye!");
     }
 }
