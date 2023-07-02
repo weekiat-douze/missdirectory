20dbfa605f13c8338baaabebdccf25288c9f476e
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 21:08:52 +0800

    Add ExecutionException class

---
+++ src/main/java/org/missdirectory/exceptions/ExecuteException.java
@@ -0,0 +1,10 @@
+package org.missdirectory.exceptions;
+
+/**
+ * Exception that represents unsuccessful execution.
+ */
+public class ExecuteException extends Exception{
+    public ExecuteException(String message) {
+        super(message);
+    }
+}
