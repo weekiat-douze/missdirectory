32a97ed696f6c81e2e94e2c48fd77a4ef1ba34bb
weekiat-douze <lumweekiat98@gmail.com>
2023-07-02 18:49:21 +0800

    Add methods in Directory to add/get subdirectories

--- src/main/java/org/missdirectory/model/Directory.java
+++ src/main/java/org/missdirectory/model/Directory.java
@@ -1,11 +1,41 @@
 package org.missdirectory.model;
 
+import java.util.HashMap;
+
+/**
+ * Represents a Directory.
+ */
 public class Directory {
     private String directoryName;
-
+    private HashMap<String, Directory> subDirectories;
 
     public Directory(String directoryName) {
         this.directoryName = directoryName;
+        this.subDirectories = new HashMap<>();
+    }
+
+    /**
+     * Create a subdirectory of this directory.
+     * @param subDirName Name of new directory.
+     * @return Whether adding of subdirectory is successful.
+     */
+    public boolean addSubdirectory(String subDirName) {
+        boolean exists = this.subDirectories.containsKey(subDirName);
+        if (exists) {
+            return false;
+        }
+        Directory newDir = new Directory(subDirName);
+        this.subDirectories.put(subDirName, newDir);
+        return true;
+    }
+
+    /**
+     * Retrieve subdirectory with the given name.
+     * @param subDirName Name of the Directory to retrieve.
+     * @return The Directory to retrieve.
+     */
+    public Directory getSubdirectory(String subDirName) {
+        return this.subDirectories.get(subDirName);
     }
 
 }
