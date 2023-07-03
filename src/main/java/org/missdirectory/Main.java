3ed471fa1c460031bd8d8d6ff9a241c75fc41c09
weekiat-douze <lumweekiat98@gmail.com>
2023-07-03 21:12:55 +0800

    Add ViewMode class

--- src/main/java/org/missdirectory/Main.java
+++ src/main/java/org/missdirectory/Main.java
@@ -1,12 +1,15 @@
 package org.missdirectory;
 
 import org.missdirectory.model.Template;
+import org.missdirectory.storage.TemplateManager;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        TemplateManager templateManager = TemplateManager.getTemplateManager();
 
         System.out.println("Hello! I am MissDirectory!\n");
         String mdMenu = "\n1. View/Modify Directory Templates" +
@@ -19,20 +22,26 @@ public class Main {
         while(!userInput.equals("4") && !userInput.equals("quit")) {
             switch (userInput) {
                 case "1":
-                    System.out.println("one");
+                    ViewMode vm = new ViewMode(templateManager);
+                    vm.run(reader);
                     break;
                 case "2":
                     System.out.print("Template name: ");
                     String templateName = reader.nextLine();
                     EditorMode em = new EditorMode(new Template(templateName));
-                    em.run();
+                    Template newTemplate = em.run(reader);
+                    if (newTemplate != null) {
+                        templateManager.setTemplate(newTemplate.getTemplateName(), newTemplate);
+                    }
                     break;
                 default:
-                    System.out.println("Please select one of the options." + mdMenu);
+                    System.out.println("Please select one of the options.");
             }
+            System.out.println(mdMenu);
             userInput = reader.nextLine();
         }
 
+        reader.close();
         System.out.println("Good bye!");
     }
 }
---
+++ src/main/java/org/missdirectory/ViewMode.java
@@ -0,0 +1,55 @@
+package org.missdirectory;
+
+import org.missdirectory.model.Template;
+import org.missdirectory.storage.TemplateManager;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Class represents the directory template viewing interface.
+ */
+public class ViewMode {
+    private TemplateManager templateManager;
+    public ViewMode(TemplateManager templateManager) {
+        this.templateManager = templateManager;
+    }
+
+    public void run(Scanner reader) {
+        System.out.println("Select via index to modify: ");
+        ArrayList<String> templateListString = this.templateManager.getTemplateList();
+        String listFormatted = formatTemplateList(templateListString);
+        System.out.println(listFormatted);
+        String userInput = reader.nextLine();
+        while(!userInput.equals("exit")) {
+            try {
+                int index = Integer.parseInt(userInput) - 1;
+                String templateName = templateListString.get(index);
+                Template selectedTemplate = this.templateManager.getTemplate(templateName);
+                EditorMode em = new EditorMode(selectedTemplate);
+                Template editedTemplate = em.run(reader);
+                templateManager.setTemplate(editedTemplate.getTemplateName(), editedTemplate);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter a valid index.");
+            } catch (IndexOutOfBoundsException iob) {
+                System.out.println("Please enter a valid index.");
+            }
+            System.out.println(listFormatted);
+            userInput = reader.nextLine();
+        }
+
+
+    }
+
+    private String formatTemplateList(ArrayList<String> templateListString) {
+        String formattedString = "";
+        for (int i = 0; i < templateListString.size(); i++) {
+            formattedString += i+1 + ". " + templateListString.get(i);
+            if (i+1 != templateListString.size()) {
+                formattedString += "\n";
+            }
+        }
+        return formattedString;
+    }
+
+}
