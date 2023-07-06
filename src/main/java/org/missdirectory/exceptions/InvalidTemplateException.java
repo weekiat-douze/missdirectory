e7a6d53eb24f2ea2638eaee0d4b726af47d5e9e6
weekiat-douze <lumweekiat98@gmail.com>
2023-07-06 20:59:21 +0800

    Add InvalidTemplateException class

---
+++ src/main/java/org/missdirectory/exceptions/InvalidTemplateException.java
@@ -0,0 +1,7 @@
+package org.missdirectory.exceptions;
+
+public class InvalidTemplateException extends Exception{
+    public InvalidTemplateException(String message) {
+        super(message);
+    }
+}
