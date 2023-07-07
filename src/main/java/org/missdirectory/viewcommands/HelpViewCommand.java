898a52bca7453ed4e77387bebfb370d47275c9af
weekiat-douze <lumweekiat98@gmail.com>
2023-07-07 15:32:54 +0800

    Change HelpViewCommand summary format

--- src/main/java/org/missdirectory/viewcommands/HelpViewCommand.java
+++ src/main/java/org/missdirectory/viewcommands/HelpViewCommand.java
@@ -12,13 +12,13 @@ public class HelpViewCommand extends ViewCommand{
     public static final String HINT = "Run `help` for summary of commands";
 
     public static final String HELP = "Example usage:\n"
-            + "`tree {index}` to see the template's tree structure\n"
-            + "`delete {index}` to delete the template\n"
-            + "`edit {index}` to edit a template\n"
-            + "`exit` to go back to main menu";
+            + "  `tree INDEX` — see the template's tree structure\n"
+            + "  `delete INDEX` — delete the template\n"
+            + "  `edit INDEX` — edit a template\n"
+            + "  `exit` — go back to main menu";
 
     @Override
     public void execute(TemplateManager templateManager, Scanner reader) throws ExecuteException {
-        System.out.println(HELP);
+        System.out.println(HelpViewCommand.HELP);
     }
 }
